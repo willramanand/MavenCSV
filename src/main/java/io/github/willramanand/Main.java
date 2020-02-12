f41806cc49a1db0cb4a2677967b2c666549ff649
willr <willramanand@gmail.com>
2020-02-12 11:13:45 -0500

    Fix possible empty unique values.

--- src/main/java/io/github/willramanand/Main.java
+++ src/main/java/io/github/willramanand/Main.java
@@ -4,6 +4,8 @@ package io.github.willramanand;
 import com.google.gson.Gson;
 import com.google.gson.stream.JsonReader;
 import com.opencsv.CSVReader;
+import org.sqlite.SQLiteException;
+
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
@@ -18,130 +20,144 @@ import java.sql.SQLException;
 
 public class Main {
 
-  static Connection conn = null;
+    static Connection conn = null;
 
-  public static void main(String[] args) throws SQLException, IOException, URISyntaxException {
-    System.out.println("---------Iteration 1---------");
-    readExampleFile();
+    public static void main(String[] args) throws SQLException, IOException, URISyntaxException {
+        System.out.println("---------Iteration 1---------");
+        readExampleFile();
 
-    System.out.println("\n---------Iteration 2----------");
-    System.out.println("Reading from CSV and inserting into database.");
-    CSVtoDB();
+        System.out.println("\n---------Iteration 2----------");
+        System.out.println("Reading from CSV and inserting into database.");
+        CSVtoDB();
 
-    System.out.println("Reading from JSON and inserting into database.");
-    JSONtoDB();
+        System.out.println("Reading from JSON and inserting into database.");
+        JSONtoDB();
 
-  }
-
-  public static void readExampleFile() {
-    try {
-      // Read file path and use as input for csvreader.
-      Reader reader = Files
-          .newBufferedReader(Paths.get(ClassLoader.getSystemResource("SEOExample.csv").toURI()));
-      CSVReader csvReader = new CSVReader(reader);
-
-      // Read initial categories at header
-      String[] categories;
-      categories = csvReader.readNext();
-
-      // Begin reading through records
-      String[] record;
-      int count = 1;
-      while ((record = csvReader.readNext()) != null) {
-        System.out.println("============= Entry " + count + " ================");
-        System.out.println(categories[0] + ": " + record[0]);
-        System.out.println(categories[1] + ": " + record[1]);
-        System.out.println(categories[2] + ": " + record[2]);
-        System.out.println(categories[3] + ": " + record[3]);
-        System.out.println(categories[4] + ": " + record[4]);
-        System.out.println(categories[5] + ": " + record[5]);
-        System.out.println(categories[6] + ": " + record[6]);
-        System.out.println(categories[7] + ": " + record[7]);
-        System.out.println(categories[8] + ": " + record[8]);
-        System.out.println(categories[9] + ": " + record[9]);
-        System.out.println("======================================");
-        count++;
-      }
-    } catch (IOException | URISyntaxException e) {
-      System.out.println("Error finding file!");
-      e.printStackTrace();
     }
-  }
 
-  public static void CSVtoDB() throws SQLException {
-    initializeDb();
-    try {
-      // Read file path and use as input for csvreader.
-      Reader reader = Files
-          .newBufferedReader(
-              Paths.get(ClassLoader.getSystemResource("bookstore_report2.csv").toURI()));
-      CSVReader csvReader = new CSVReader(reader);
+    public static void readExampleFile() {
+        try {
+            // Read file path and use as input for csvreader.
+            Reader reader = Files
+                    .newBufferedReader(Paths.get(ClassLoader.getSystemResource("SEOExample.csv").toURI()));
+            CSVReader csvReader = new CSVReader(reader);
 
-      // Read initial categories at header
-      String[] categories;
-      categories = csvReader.readNext();
+            // Read initial categories at header
+            String[] categories;
+            categories = csvReader.readNext();
 
-      // Begin reading through records
-      String[] record;
-      while ((record = csvReader.readNext()) != null) {
-        // Create query
-        String sql = "INSERT INTO book(isbn, book_title, author_name, publisher_name ) VALUES (?, ?, ?, ? )";
-
-        // Set as prepared statement to put dynamic values
-        PreparedStatement preparedStatement = conn.prepareStatement(sql);
-
-        // Insert dynamic values
-        preparedStatement.setString(1, record[0]);
-        preparedStatement.setString(2, record[1]);
-        preparedStatement.setString(3, record[2]);
-        preparedStatement.setString(4, record[3]);
-
-        // Execute query
-        preparedStatement.executeUpdate();
-        preparedStatement.close();
-      }
-
-    } catch (IOException | URISyntaxException | SQLException e) {
-      e.printStackTrace();
+            // Begin reading through records
+            String[] record;
+            int count = 1;
+            while ((record = csvReader.readNext()) != null) {
+                System.out.println("============= Entry " + count + " ================");
+                System.out.println(categories[0] + ": " + record[0]);
+                System.out.println(categories[1] + ": " + record[1]);
+                System.out.println(categories[2] + ": " + record[2]);
+                System.out.println(categories[3] + ": " + record[3]);
+                System.out.println(categories[4] + ": " + record[4]);
+                System.out.println(categories[5] + ": " + record[5]);
+                System.out.println(categories[6] + ": " + record[6]);
+                System.out.println(categories[7] + ": " + record[7]);
+                System.out.println(categories[8] + ": " + record[8]);
+                System.out.println(categories[9] + ": " + record[9]);
+                System.out.println("======================================");
+                count++;
+            }
+        } catch (IOException | URISyntaxException e) {
+            System.out.println("Error finding file!");
+            e.printStackTrace();
+        }
     }
-   closeDb();
-  }
 
-  public static void JSONtoDB() throws FileNotFoundException, SQLException {
-    initializeDb();
+    public static void CSVtoDB() throws SQLException {
+        initializeDb();
+        try {
+            // Read file path and use as input for csvreader.
+            Reader reader = Files
+                    .newBufferedReader(
+                            Paths.get(ClassLoader.getSystemResource("bookstore_report2.csv").toURI()));
+            CSVReader csvReader = new CSVReader(reader);
 
-    Gson gson = new Gson();
-    JsonReader jread = new JsonReader(new FileReader("src\\main\\resources\\authors.json"));
-    AuthorParser[] authors = gson.fromJson(jread, AuthorParser[].class);
+            // Read initial categories at header
+            String[] categories;
+            categories = csvReader.readNext();
 
-    for (var author : authors) {
-      // Create query
-      String sql = "INSERT INTO author(author_name, author_email, author_url ) VALUES (?, ?, ? )";
+            // Begin reading through records
+            String[] record;
+            while ((record = csvReader.readNext()) != null) {
+                // Create query
+                String sql = "INSERT INTO book(isbn, book_title, author_name, publisher_name ) VALUES (?, ?, ?, ? )";
 
-      // Set as prepared statement to put dynamic values
-      PreparedStatement preparedStatement = conn.prepareStatement(sql);
+                // Set as prepared statement to put dynamic values
+                PreparedStatement preparedStatement = conn.prepareStatement(sql);
 
-      // Insert dynamic values
-      preparedStatement.setString(1, author.getName());
-      preparedStatement.setString(2, author.getEmail());
-      preparedStatement.setString(3, author.getURL());
-      // Execute query
-      preparedStatement.executeUpdate();
+                // Insert dynamic values
+                preparedStatement.setString(1, record[0]);
+                preparedStatement.setString(2, record[1]);
+                preparedStatement.setString(3, record[2]);
+                preparedStatement.setString(4, record[3]);
 
-      preparedStatement.close();
+                // Execute query
+                preparedStatement.executeUpdate();
+                preparedStatement.close();
+            }
+
+        } catch (IOException | URISyntaxException e) {
+            e.printStackTrace();
+        } catch (SQLiteException e) {
+          System.out.println("This data already exists in the database!");
+        }
+        closeDb();
     }
-    closeDb();
-  }
 
-  public static void initializeDb() throws SQLException {
-    String url = "jdbc:sqlite:./src/main/resources/db/bookstore.db";
-    System.out.println("Connection to Database has been established");
-    conn = DriverManager.getConnection(url);
-  }
+    public static void JSONtoDB() throws FileNotFoundException, SQLException {
+        initializeDb();
 
-  public static void closeDb() throws SQLException {
-    conn.close();
-    System.out.println("Connection to database has been successfully closed!");
-  }
+        Gson gson = new Gson();
+        JsonReader jread = new JsonReader(new FileReader("src\\main\\resources\\authors.json"));
+        AuthorParser[] authors = gson.fromJson(jread, AuthorParser[].class);
+
+        for (var author : authors) {
+            // Create query
+            String sql = "INSERT INTO author(author_name, author_email, author_url ) VALUES (?, ?, ? )";
+
+            // Set as prepared statement to put dynamic values
+            PreparedStatement preparedStatement = conn.prepareStatement(sql);
+            try {
+                // Insert dynamic values
+                preparedStatement.setString(1, author.getName());
+
+                if (!author.getEmail().isEmpty()) {
+                    preparedStatement.setString(2, author.getEmail());
+                } else {
+                    preparedStatement.setString(2, author.getName() + ".NO_EMAIL");
+                }
+                if (!author.getURL().isEmpty()) {
+                    preparedStatement.setString(3, author.getURL());
+                } else {
+                    preparedStatement.setString(3, author.getName() + ".NO_URL");
+                }
+
+                // Execute query
+                preparedStatement.executeUpdate();
+            } catch (SQLiteException e) {
+                System.out.println("This data already exists within the database!");
+            }
+            preparedStatement.close();
+        }
+        closeDb();
+    }
+
+    public static void initializeDb() throws SQLException {
+        String url = "jdbc:sqlite:./src/main/resources/db/bookstore.db";
+        System.out.println("Connection to Database has been established");
+        conn = DriverManager.getConnection(url);
+    }
+
+    public static void closeDb() throws SQLException {
+        conn.close();
+        System.out.println("Connection to database has been successfully closed!");
+    }
 }
 
